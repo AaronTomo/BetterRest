2abd2f99b5603120b07238c43d4364b9a3e65679
Aaron Thompson <aaronseanthompson@icloud.com>
2024-06-27 20:28:49 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aaron Thompson on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
